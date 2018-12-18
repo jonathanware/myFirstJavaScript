3640d5632a3c60d1df40a57082d4c15296de354c
jonathanware <jonathan.ware@gmail.com>
2018-12-18 08:23:24 -0700

    added variable stuff

--- myScript.js
+++ myScript.js
@@ -1,3 +1,23 @@
+//This function changes the text in the demo paragraph
+
 function myFunction () {
     document.getElementById('demo').innerHTML = "Paragraph Changed";
 }
+
+//Defining variables
+
+//var x = 5;
+//var y = 6;
+//var z = x + y;
+
+var x, y, z;
+x = 5;
+y = 6;
+z = x + y;
+
+//This function changes the text in the math paragraph
+
+function mathFunction () {
+    document.getElementById('math').innerHTML = z;
+}
+
